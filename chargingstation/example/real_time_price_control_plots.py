a180c74b57526015482b6d2e75937e43785f9f2a
AkshayThiru <akshay_t@berkeley.edu>
2024-09-14 17:57:13 -0700

    Refactor plotting files

--- chargingstation/example/real_time_price_control_plots.py
+++ chargingstation/example/real_time_price_control_plots.py
@@ -1,141 +1,317 @@
+import os
 import pickle
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
+from chargingstation.charging_station import ChargingStationConstants
+from chargingstation.settings import MIN_INITIAL_SOC
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+COL_WIDTH = 3.54  # [inch].
+FIG_DPI = 200
+SAVE_DPI = 1000  # >= 600.
+SAVE_FIG = True
+
+# mpl.rcParams["mathtext.fontset"] = "stix"
+mpl.rcParams["mathtext.fontset"] = "cm"
+mpl.rcParams["font.family"] = "STIXGeneral"
+# mpl.rcParams["font.family"] = "cmr10"
+
 
 def _plot_graphs(logs: dict) -> None:
-    consts = logs["constants"]
+    consts: ChargingStationConstants = logs["constants"]
     consts_bi = consts.bimpc_consts
-    consts_s, consts_l = consts.small_bat_consts, consts.large_bat_consts
+    consts_s = consts.small_EV_consts
+    consts_l = consts.large_EV_consts
     Tf = consts.simulation_length - 1
-    nr = consts.nrobots_per_bat_type
-    B = (consts_s.theta + consts_l.theta) * nr
+    M_2 = consts.nEVs_per_EV_type
+    B = (consts_s.theta + consts_l.theta) * M_2
     demand = consts.demand[: Tf + 1]
 
-    # Robot distributions.
-    Nr_s, Nr_l = logs["statistics"]["Nr_s"], logs["statistics"]["Nr_l"]
-    # Robustness bounds.
-    beta_s, beta_l = logs["bounds"]["beta_s"], logs["bounds"]["beta_l"]
-    # Number of robots charged.
-    ncharged_s, ncharged_l = (
-        logs["statistics"]["num_charged_s"],
-        logs["statistics"]["num_charged_l"],
-    )
-
-    # Energy generated.
-    u_gen = logs["inputs"]["u_gen"]
-    # (Actual / predicted) robot consumptions.
-    w_s, w_l = logs["inputs"]["w_s"], logs["inputs"]["w_l"]
-    w_s_bi, w_l_bi = logs["inputs"]["w_s_bimpc"], logs["inputs"]["w_l_bimpc"]
-    # Total normalized (actual / predicted) energy consumption by robots (and errors).
-    total_w_s = consts_s.theta * np.sum(Nr_s * w_s, axis=0) / B
-    total_w_l = consts_l.theta * np.sum(Nr_l * w_l, axis=0) / B
-    total_w_s_bi = consts_s.theta * np.sum(Nr_s * w_s_bi, axis=0) / B
-    total_w_l_bi = consts_l.theta * np.sum(Nr_l * w_l_bi, axis=0) / B
-    err_w_s = consts_s.theta * np.sum(Nr_s * beta_s, axis=0) / B
-    err_w_l = consts_l.theta * np.sum(Nr_l * beta_l, axis=0) / B
-
-    # Normalized (actual / predicted) battery charge and input.
-    u_bat_max = consts_bi.u_bat_max
-    xi_bat_max = consts_bi.xi_bat_max
-    xi_bat = logs["states"]["xi_bat"]
-    xi0_bat = xi_bat[0]
-    u_bat = xi_bat[1:] - xi_bat[:-1]
-    u_bat_bi = u_gen - demand / B - total_w_s_bi - total_w_l_bi
-    xi_bat_bi = np.zeros((Tf + 1,))
-    xi_bat_bi[0] = xi0_bat
-    xi_bat_bi[1:] = xi_bat[:-1] + u_bat_bi[:-1]
-
-    # Prices.
-    avg_price_s = np.sum(Nr_s * logs["prices"]["avg_price_s"], axis=0) / nr
-    avg_price_l = np.sum(Nr_l * logs["prices"]["avg_price_s"], axis=0) / nr
-    price_red_s = np.nan_to_num(np.zeros_like(logs["prices"]["price_red_s"]))
-    price_red_l = np.nan_to_num(np.zeros_like(logs["prices"]["price_red_l"]))
-    price_before_reg_s = avg_price_s + np.sum(Nr_s * price_red_s, axis=0) / nr
-    price_before_reg_l = avg_price_l + np.sum(Nr_l * price_red_l, axis=0) / nr
-
-    ### Statistics.
-    print(f"# small robots charged: {ncharged_s}")
-    print(f"# large robots charged: {ncharged_l}")
-
-    ### Plots.
     t = np.arange(Tf)
 
-    _, ax = plt.subplots(2, 2)
-    # w_s with error.
-    ax[0][0].plot(t, total_w_s[:Tf], "-b", label="actual w_s")
-    ax[0][0].plot(t, total_w_s_bi[:Tf], "-r", label="pred w_s")
-    total_w_s_min = np.max(((total_w_s_bi - err_w_s)[:Tf], np.zeros((Tf,))), axis=0)
-    total_w_s_max = np.min(
+    # EV distributions.
+    Mp_s, Mp_l = logs["statistics"]["Mp_s"], logs["statistics"]["Mp_l"]
+    # Robustness bounds.
+    beta_s, beta_l = logs["bounds"]["beta_s"], logs["bounds"]["beta_l"]
+    # Number of EVs charged.
+    ncharged_s, ncharged_l = (
+        logs["statistics"]["ncharged_s"],
+        logs["statistics"]["ncharged_l"],
+    )
+    # Price solver iterations.
+    niter_s, niter_l = (
+        logs["statistics"]["niter_s"],
+        logs["statistics"]["niter_l"],
+    )
+    niter_s = niter_s[niter_s >= 1]
+    niter_l = niter_l[niter_l >= 1]
+
+    # Electricity generated.
+    u_g_max = consts_bi.u_g_max
+    u_g = logs["inputs"]["u_g"]
+    # (Actual / predicted) EV electricity consumptions.
+    w_s, w_l = logs["inputs"]["w_s"], logs["inputs"]["w_l"]
+    w_hat_s, w_hat_l = logs["inputs"]["w_hat_s"], logs["inputs"]["w_hat_l"]
+    # Total normalized (actual / predicted) electricity consumption by EVs (and errors).
+    total_w_s = consts_s.theta * np.sum(Mp_s * w_s, axis=0) / B
+    total_w_l = consts_l.theta * np.sum(Mp_l * w_l, axis=0) / B
+    total_w_hat_s = consts_s.theta * np.sum(Mp_s * w_hat_s, axis=0) / B
+    total_w_hat_l = consts_l.theta * np.sum(Mp_l * w_hat_l, axis=0) / B
+    err_w_s = consts_s.theta * np.sum(Mp_s * beta_s, axis=0) / B
+    err_w_l = consts_l.theta * np.sum(Mp_l * beta_l, axis=0) / B
+    total_w_hat_s_min = np.max(
+        ((total_w_hat_s - err_w_s)[:Tf], np.zeros((Tf,))), axis=0
+    )
+    total_w_hat_s_max = np.min(
         (
-            (total_w_s_bi + err_w_s)[:Tf],
-            nr * consts_s.w_max * consts_s.theta / B * np.ones((Tf)),
+            (total_w_hat_s + err_w_s)[:Tf],
+            M_2 * consts_s.w_max * consts_s.theta / B * np.ones((Tf)),
         ),
         axis=0,
     )
-    ax[0][0].fill_between(t, total_w_s_min, total_w_s_max, alpha=0.2, color="r")
-    ax[0][0].plot(t, nr * consts_s.w_max * consts_s.theta / B * np.ones((Tf,)), "--b")
-    ax[0][0].legend()
-
-    # w_l with error.
-    ax[0][1].plot(t, total_w_l[:Tf], "-b", label="actual w_l")
-    ax[0][1].plot(t, total_w_l_bi[:Tf], "-r", label="pred w_l")
-    total_w_l_min = np.max(((total_w_l_bi - err_w_l)[:Tf], np.zeros((Tf,))), axis=0)
-    total_w_l_max = np.min(
+    total_w_hat_l_min = np.max(
+        ((total_w_hat_l - err_w_l)[:Tf], np.zeros((Tf,))), axis=0
+    )
+    total_w_hat_l_max = np.min(
         (
-            (total_w_l_bi + err_w_l)[:Tf],
-            nr * consts_l.w_max * consts_l.theta / B * np.ones((Tf)),
+            (total_w_hat_l + err_w_l)[:Tf],
+            M_2 * consts_l.w_max * consts_l.theta / B * np.ones((Tf)),
         ),
         axis=0,
     )
-    ax[0][1].fill_between(t, total_w_l_min, total_w_l_max, alpha=0.2, color="r")
-    ax[0][1].plot(t, nr * consts_l.w_max * consts_l.theta / B * np.ones((Tf,)), "--b")
-    ax[0][1].legend()
+    total_w = total_w_s + total_w_l
+    total_w_hat = total_w_hat_s + total_w_hat_l
+    total_w_hat_min = total_w_hat_s_min + total_w_hat_l_min
+    total_w_hat_max = total_w_hat_s_max + total_w_hat_l_max
+    total_w_max = (
+        M_2
+        * (consts_s.w_max * consts_s.theta + consts_l.w_max * consts_l.theta)
+        / B
+        * np.ones((Tf,))
+    )
 
-    # Energy generated, demand, and battery state.
-    ax[1][0].plot(t, np.zeros((Tf,)), "-.y")
-    ax[1][0].plot(t, demand[:Tf] / B, "-r", label="demand")
-    ax[1][0].plot(t, u_gen[:Tf], "-b", label="u_gen")
-    ax[1][0].plot(t, (total_w_s + total_w_l)[:Tf], "-m", label="w")
-    # ax[1][0].plot(t, u_bat[:Tf], "-k", label="u_bat")
-    # ax[1][0].plot(t, u_bat_bi[:Tf], "--k", label="u_bat")
-    u_bat_bi_min = np.max(
-        (u_bat_bi[:Tf] - err_w_s[:Tf] - err_w_l[:Tf], -u_bat_max * np.ones((Tf,))),
+    # Normalized (actual / predicted) battery charge and input.
+    u_b_max = consts_bi.u_b_max
+    x_max = consts_bi.x_max
+    x = logs["states"]["x"]
+    x0 = x[0]
+    u_b = x[1:] - x[:-1]
+    u_hat_b = u_g - demand / B - total_w_hat_s - total_w_hat_l
+    x_hat = np.zeros((Tf + 1,))
+    x_hat[0] = x0
+    x_hat[1:] = x[:-1] + u_hat_b[:-1]
+    u_hat_b_bi_min = np.max(
+        (u_hat_b[:Tf] - err_w_s[:Tf] - err_w_l[:Tf], -u_b_max * np.ones((Tf,))),
         axis=0,
     )
-    u_bat_bi_max = np.min(
-        (u_bat_bi[:Tf] + err_w_s[:Tf] + err_w_l[:Tf], u_bat_max * np.ones((Tf,))),
+    u_hat_b_max = np.min(
+        (u_hat_b[:Tf] + err_w_s[:Tf] + err_w_l[:Tf], u_b_max * np.ones((Tf,))),
         axis=0,
     )
-    # ax[1][0].fill_between(t, u_bat_bi_min, u_bat_bi_max, alpha=0.2, color="grey")
-    ax[1][0].plot(t, xi_bat[:Tf], "-g", label="xi_bat")
-    ax[1][0].plot(t, xi_bat_bi[:Tf], "--g")
-    xi_bat_bi_min = np.max(
-        ((xi_bat_bi - err_w_s - err_w_l)[:Tf], np.zeros((Tf))), axis=0
+    x_hat_min = np.max(((x_hat - err_w_s - err_w_l)[:Tf], np.zeros((Tf))), axis=0)
+    x_hat_max = np.min(
+        ((x_hat + err_w_s + err_w_l)[:Tf], x_max * np.ones((Tf,))), axis=0
     )
-    xi_bat_bi_max = np.min(
-        ((xi_bat_bi + err_w_s + err_w_l)[:Tf], xi_bat_max * np.ones((Tf,))), axis=0
-    )
-    ax[1][0].fill_between(t, xi_bat_bi_min, xi_bat_bi_max, alpha=0.2, color="g")
-    ax[1][0].legend()
 
-    # Average prices paid.
-    ax[1][1].plot(t, avg_price_s[:Tf], "-r", label="price_s")
-    # ax[1][1].plot(t, price_before_reg_s[: Tf], "--r")
-    ax[1][1].plot(t, avg_price_l[:Tf], "-b", label="price_l")
-    # ax[1][1].plot(t, price_before_reg_l[: Tf], "--b")
-    ax[1][1].legend()
+    # Prices.
+    avg_price_s = np.sum(Mp_s * logs["prices"]["avg_price_s"], axis=0) / M_2
+    avg_price_l = np.sum(Mp_l * logs["prices"]["avg_price_s"], axis=0) / M_2
+    price_red_s = np.nan_to_num(np.zeros_like(logs["prices"]["price_red_s"]))
+    price_red_l = np.nan_to_num(np.zeros_like(logs["prices"]["price_red_l"]))
+    price_before_reg_s = avg_price_s + np.sum(Mp_s * price_red_s, axis=0) / M_2
+    price_before_reg_l = avg_price_l + np.sum(Mp_l * price_red_l, axis=0) / M_2
+
+    ### Statistics.
+    ncharged_s_max = M_2 * (Tf - 1) / ((consts_s.y_max - MIN_INITIAL_SOC) / consts_s.w_max)
+    ncharged_l_max = M_2 * (Tf - 1) / ((consts_l.y_max - MIN_INITIAL_SOC) / consts_l.w_max)
+    print(f"# small robots charged: {ncharged_s:6d}, ({100 * ncharged_s / ncharged_s_max:6.2f}%)")
+    print(f"# large robots charged: {ncharged_l:6d}, ({100 * ncharged_l / ncharged_l_max:6.2f}%)")
+
+    print(f"Average # iterations for small robots: {np.mean(niter_s):7.2f}")
+    print(f"Average # iterations for large robots: {np.mean(niter_l):7.2f}")
+
+    ### Plotting.
+    # Figure properties.
+    font_size = 8
+    font_dict = {
+        "fontsize": font_size,  # [pt]
+        "fontstyle": "normal",
+        "fontweight": "normal",
+    }
+    axis_margins = 0.05
+
+    # Input plots.
+    # Charging rate with error.
+    fig_height = 1.4  # [inch].
+    fig, ax = plt.subplots(
+        1,
+        1,
+        figsize=(COL_WIDTH, fig_height),
+        dpi=FIG_DPI,
+        sharey=False,
+        layout="constrained",
+    )
+    ax.spines.top.set_visible(False)
+    ax.spines.right.set_visible(False)
+    ax.plot(t, total_w[:Tf], "-b", lw=1, label=r"$w$")
+    ax.plot(t, total_w_hat[:Tf], "--r", lw=1, label=r"$\hat{w}$")
+    ax.fill_between(
+        t,
+        total_w_hat_min,
+        total_w_hat_max,
+        alpha=0.1,
+        color="r",
+        lw=1,
+        label="error\nbound",
+    )
+    ax.plot(t, total_w_max, "-.b", lw=1, label=r"$w_{\text{max}}$")
+    ax.grid(axis="y", lw=0.25, alpha=0.5)
+    ax.set_xlabel(r"time $\ (\text{hrs})$", **font_dict)
+    ax.set_ylabel(r"normalized aggregate" "\n" r"EV charging rate $\ ()$", **font_dict)
+    ax.legend(
+        loc="center right",
+        fontsize=font_size,
+        framealpha=0.5,
+        fancybox=False,
+        edgecolor="black",
+        labelspacing=0.15,
+    )
+    ax.set_xticks([0, 12, 24, 36, 47])
+    ax.tick_params(axis="both", which="major", labelsize=font_size)
+    ax.margins(axis_margins, axis_margins)
 
     plt.show()
+
+    if SAVE_FIG:
+        fig.savefig(
+            DIR_PATH + "/../plots/aggregate_ev_charging_rate.png", dpi=SAVE_DPI
+        )  # , bbox_inches='tight')
+
+    # Electricity generated and demand.
+    fig_height = 1.5  # [inch].
+    fig, ax = plt.subplots(
+        1,
+        1,
+        figsize=(COL_WIDTH, fig_height),
+        dpi=FIG_DPI,
+        sharey=False,
+        layout="constrained",
+    )
+    ax.spines.top.set_visible(False)
+    ax.spines.right.set_visible(False)
+    ax.plot(t, u_g[:Tf], "-b", lw=1, label=r"$u^{\text{g}}$", zorder=3)
+    ax.plot(
+        t,
+        u_g_max * np.ones((Tf,)),
+        "-.b",
+        lw=1,
+        label=r"$u^{\text{g}}_\text{max}$",
+        zorder=2,
+    )
+    ax.plot(t, demand[:Tf] / B, "--r", lw=1, label="external\ndemand", zorder=1)
+    # ax.plot(t, total_w[:Tf], "-m", lw=1, label=r"$w$")
+    # ax.plot(t, total_w_max[:Tf], "-.m", lw=1, label=r"$w_\text{max}$")
+    # ax.plot(t, u_b[:Tf], "-k", lw=1, label=r"$u^b$")
+    # ax.plot(t, u_hat_b[:Tf], "--k", lw=1, label=r"$\hat{u}^b$")
+    # ax.fill_between(t, u_hat_b_min, u_hat_b_max, alpha=0.2, color="grey", lw=1)
+    # ax.plot(t, x[:Tf], "-g", lw=1, label=r"$x$")
+    # ax.plot(t, x_hat[:Tf], "--m", lw=1, label=r"$\hat{x}$")
+    # ax.plot(t, x_max * np.ones((Tf,)), "-.g", lw=1, label=r"$x_\text{max}$")
+    # ax.fill_between(t, x_hat_b_min, x_hat_b_max, alpha=0.2, color="g", lw=1)
+    ax.grid(axis="y", lw=0.25, alpha=0.5)
+    ax.set_xlabel(r"time $\ (\text{hrs})$", **font_dict)
+    ax.set_ylabel(
+        r"normalized demand and" "\n" r"energy generation $\ ()$", **font_dict
+    )
+    leg = ax.legend(
+        loc="upper right",
+        bbox_to_anchor=(1, 0.98),
+        ncol=2,
+        fontsize=font_size,
+        framealpha=0.5,
+        fancybox=False,
+        edgecolor="black",
+    )
+    leg.legend_handles[0].set_ydata([6] * 3)
+    leg.texts[0].set_y(60)
+    leg.legend_handles[1].set_ydata([6] * 3)
+    leg.texts[1].set_y(60)
+    leg._legend_box.set_height(300)
+    ax.set_xticks([0, 12, 24, 36, 47])
+    ax.tick_params(axis="both", which="major", labelsize=font_size)
+    ax.margins(axis_margins, axis_margins)
+
+    plt.show()
+
+    if SAVE_FIG:
+        fig.savefig(
+            DIR_PATH + "/../plots/demand_energy_generation.png", dpi=SAVE_DPI
+        )  # , bbox_inches='tight')
+
+    # Storage battery state.
+    fig_height = 1.5  # [inch].
+    fig, ax = plt.subplots(
+        1,
+        1,
+        figsize=(COL_WIDTH, fig_height),
+        dpi=FIG_DPI,
+        sharey=False,
+        layout="constrained",
+    )
+    ax.spines.top.set_visible(False)
+    ax.spines.right.set_visible(False)
+    # ax.plot(t, u_b[:Tf], "-k", lw=1, label=r"$u^b$")
+    # ax.plot(t, u_hat_b[:Tf], "--k", lw=1, label=r"$\hat{u}^b$")
+    # ax.fill_between(t, u_hat_b_min, u_hat_b_max, alpha=0.2, color="grey", lw=1)
+    ax.plot(t, x[:Tf], "-b", lw=1, label=r"$x$")
+    ax.plot(t, x_hat[:Tf], "--r", lw=1, label=r"$\hat{x}$")
+    ax.plot(t, x_max * np.ones((Tf,)), "-.b", lw=1, label=r"$x_\text{max}$")
+    ax.fill_between(
+        t,
+        x_hat_min,
+        x_hat_max,
+        alpha=0.1,
+        color="r",
+        lw=1,
+        label="error\nbound",
+    )
+    ax.grid(axis="y", lw=0.25, alpha=0.5)
+    ax.set_xlabel(r"time $\ (\text{hrs})$", **font_dict)
+    ax.set_ylabel(r"normalized storage" "\n" r"battery state $\ ()$", **font_dict)
+    leg = ax.legend(
+        loc="upper right",
+        bbox_to_anchor=(1, 0.98),
+        ncol=2,
+        fontsize=font_size,
+        framealpha=0.5,
+        fancybox=False,
+        edgecolor="black",
+    )
+    # leg.legend_handles[0].set_ydata([6] * 3)
+    # leg.texts[0].set_y(9)
+    # leg.legend_handles[1].set_ydata([6] * 3)
+    # leg.texts[1].set_y(9)
+    # leg._legend_box.set_height(57)
+    ax.set_xticks([0, 12, 24, 36, 47])
+    ax.tick_params(axis="both", which="major", labelsize=font_size)
+    ax.margins(axis_margins, axis_margins)
+
+    plt.show()
+
+    if SAVE_FIG:
+        fig.savefig(
+            DIR_PATH + "/../plots/storage_battery_state.png", dpi=SAVE_DPI
+        )  # , bbox_inches='tight')
 
 
 def main() -> None:
     price_type = "linear-convex"
     # price_type = "linear"
-    file_name = (
-        "chargingstation/example/real-time-price-control_logs_" + price_type + ".pkl"
-    )
+    file_name = DIR_PATH + "/real-time-price-control_logs_" + price_type + ".pkl"
     with open(file_name, "rb") as file:
         logs = pickle.load(file)
     _plot_graphs(logs)
--- chargingstation/plots/plots.py
+++ chargingstation/plots/plots.py
@@ -9,7 +9,8 @@ from chargingstation.charging_station import ChargingStationConstants
 from chargingstation.example.real_time_price_control import \
     get_chargingstation_consts
 from chargingstation.lompc import LoMPC
-from chargingstation.price_coordinator import PriceCoordinator
+from chargingstation.price_solver import PriceSolver
+from chargingstation.settings import MIN_INITIAL_SOC
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -26,34 +27,39 @@ mpl.rcParams["font.family"] = "STIXGeneral"
 
 def _plot_robustness_bounds_large_robots(consts: ChargingStationConstants) -> None:
     N = consts.horizon_lompc
-    nr = 10
-    consts_l = consts.large_bat_consts
+    M_2 = 10
+    consts_l = consts.large_EV_consts
     lompc = LoMPC(N, consts_l)
     A = lompc.get_input_mat()
 
     # w error bound vs gamma.
-    gamma_max_arr = consts_l.s_max * np.arange(1, 0, -0.01)
+    gamma_max_arr = consts_l.y_max * np.arange(1, 0, -0.01)
     len_arr = len(gamma_max_arr)
     lmbd = 5 * consts_l.theta * np.random.random((3 * N,))
     kappa = 1e-5
     lmbd_r = consts_l.delta * kappa
     A_bar = A.T @ A + kappa * np.eye(N)  # Metric for the w-inner product.
     w_err, w0_err = np.zeros((len_arr,)), np.zeros((len_arr,))
+    w_err_max = np.zeros((len_arr,))
     w_err_bound, w0_err_bound = np.zeros((len_arr,)), np.zeros((len_arr,))
     for j in tqdm(range(len_arr)):
-        gamma_arr = gamma_max_arr[j] * np.random.random((nr,))
-        gamma_rng = gamma_max_arr[j] / 2  # (np.max(gamma_arr) - np.min(gamma_arr)) / 2
-        gamma_ref = (np.max(gamma_arr) + np.min(gamma_arr)) / 2
+        gamma_arr = gamma_max_arr[j] * np.random.random((M_2,))
+        gamma_bar = gamma_max_arr[j] / 2  # (np.max(gamma_arr) - np.min(gamma_arr)) / 2
+        gamma_sc = (np.max(gamma_arr) + np.min(gamma_arr)) / 2
+        w_opt_ref, _ = lompc.solve_lompc(lmbd, lmbd_r, gamma_sc)
         w_opt_avg = np.zeros((N,))
-        for i in range(nr):
+        w_err_max_j = 0
+        for i in range(M_2):
             w_opt, _ = lompc.solve_lompc(lmbd, lmbd_r, gamma_arr[i])
             w_opt_avg += w_opt
-        w_opt_avg = w_opt_avg / nr
-        w_opt_ref, _ = lompc.solve_lompc(lmbd, lmbd_r, gamma_ref)
+            w_err_i = np.sqrt((w_opt - w_opt_ref) @ A_bar @ (w_opt - w_opt_ref))
+            w_err_max_j = np.max((w_err_max_j, w_err_i))
+        w_opt_avg = w_opt_avg / M_2
         w_err[j] = np.sqrt((w_opt_avg - w_opt_ref) @ A_bar @ (w_opt_avg - w_opt_ref))
-        w_err_bound[j] = np.sqrt(N) * gamma_rng
+        w_err_max[j] = w_err_max_j
+        w_err_bound[j] = np.sqrt(N) * gamma_sc
         w0_err[j] = np.abs(w_opt_avg[0] - w_opt_ref[0])
-        w0_err_bound[j] = np.sqrt(N) * gamma_rng * np.min((1, 1 / np.sqrt(kappa)))
+        w0_err_bound[j] = np.sqrt(N) * gamma_sc * np.min((1, 1 / np.sqrt(kappa)))
 
     # Figure properties.
     fig_height = 1.4  # [inch].
@@ -76,9 +82,10 @@ def _plot_robustness_bounds_large_robots(consts: ChargingStationConstants) -> No
     ax.spines.top.set_visible(False)
     ax.spines.right.set_visible(False)
     ax.plot(gamma_max_arr, w_err, "-b", lw=1, label=r"$\Vert w - \hat{w}\Vert$")
+    # ax.plot(gamma_max_arr, w_err_max, "-g", lw=1, label=r"$\max_i \{\Vert w^i - \hat{w}\Vert\}$")
     ax.plot(gamma_max_arr, w_err_bound, "--r", lw=1, label=r"$\sqrt{N}\bar{\Gamma}$")
     ax.grid(axis="y", lw=0.25, alpha=0.5)
-    ax.set_xlabel(r"range of initial charge, $\bar{\Gamma} \ ()$", **font_dict)
+    ax.set_xlabel(r"range of initial normalized SoC, $\bar{\Gamma} \ ()$", **font_dict)
     ax.set_ylabel(r"error bound $\ ()$", **font_dict)
     ax.legend(
         loc="lower right",
@@ -89,7 +96,7 @@ def _plot_robustness_bounds_large_robots(consts: ChargingStationConstants) -> No
     )
     ax.tick_params(axis="both", which="major", labelsize=font_size)
     ax.set_yscale("log")
-    ax.set_xticks([0, 0.3, 0.6, consts_l.s_max])
+    ax.set_xticks([0, 0.3, 0.6, consts_l.y_max])
     ax.yaxis.set_major_locator(mpl.ticker.LogLocator(base=10, numticks=3))
     ax.yaxis.set_minor_locator(
         mpl.ticker.LogLocator(base=10, subs=np.linspace(0.2, 1, 5), numticks=50)
@@ -107,15 +114,15 @@ def _plot_robustness_bounds_large_robots(consts: ChargingStationConstants) -> No
 
 def _plot_dual_cost_decrease_large_robots(consts: ChargingStationConstants) -> None:
     N = consts.horizon_lompc
-    nr = 100
+    M_2 = 100
     lmbd_r = 0
-    consts_l = consts.large_bat_consts
+    consts_l = consts.large_EV_consts
 
-    price_coord = PriceCoordinator(N, consts_l, "linear-convex")
-    s0 = 1 / 9 * consts_l.s_max * np.random.random((nr,))
-    price_coord.set_charge_levels(s0)
+    price_solver = PriceSolver(N, consts_l, "linear-convex")
+    y0 = MIN_INITIAL_SOC + 1 / 24 * consts_l.y_max * np.random.random((M_2,))
+    price_solver.set_charge_levels(y0)
     w_ref = consts_l.w_max * np.random.random((N,))
-    _, stats = price_coord.compute_optimal_prices(w_ref, lmbd_r)
+    _, stats = price_solver.compute_optimal_prices(w_ref, lmbd_r)
     dual_cost_decrease_ac = stats["dual_cost_decrease_actual"]
     dual_cost_decrease_pred = stats["dual_cost_decrease_predicted"]
     niter = len(dual_cost_decrease_ac)
@@ -141,7 +148,7 @@ def _plot_dual_cost_decrease_large_robots(consts: ChargingStationConstants) -> N
     ax.spines.top.set_visible(False)
     ax.spines.right.set_visible(False)
     ax.plot(np.arange(niter), dual_cost_decrease_ac, "-b", lw=1, label="actual")
-    ax.plot(np.arange(niter), dual_cost_decrease_pred, "-r", lw=1, label="guaranteed")
+    ax.plot(np.arange(niter), dual_cost_decrease_pred, "--r", lw=1, label="guaranteed")
     # ax.plot(np.arange(niter), np.zeros((niter,)), "--k", lw=1)
     ax.grid(axis="y", lw=0.25, alpha=0.5)
     ax.set_xlabel(r"number of iterations", **font_dict)
@@ -176,21 +183,21 @@ def _plot_dual_cost_decrease_large_robots(consts: ChargingStationConstants) -> N
 
 def _price_comparison_large_robots(consts: ChargingStationConstants) -> None:
     N = consts.horizon_lompc
-    nr = 1
-    consts_ = consts.large_bat_consts
+    M_2 = 1
+    consts_l = consts.large_EV_consts
     lmbd_r = 0
     nsamples = 100
 
-    price_coord_l = PriceCoordinator(N, consts_, "linear")
-    price_coord_lc = PriceCoordinator(N, consts_, "linear-convex")
+    price_solver_l = PriceSolver(N, consts_l, "linear")
+    price_solver_lc = PriceSolver(N, consts_l, "linear-convex")
     avg_price_l, avg_price_lc = 0, 0
     for _ in tqdm(range(nsamples)):
-        s0 = consts_.s_max * np.random.random((nr,))
-        price_coord_l.set_charge_levels(s0)
-        price_coord_lc.set_charge_levels(s0)
-        w_ref = consts_.w_max * np.random.random((N,))
-        _, stats_l = price_coord_l.compute_optimal_prices(w_ref, lmbd_r)
-        _, stats_lc = price_coord_lc.compute_optimal_prices(w_ref, lmbd_r)
+        y0 = consts_l.y_max * np.random.random((M_2,))
+        price_solver_l.set_charge_levels(y0)
+        price_solver_lc.set_charge_levels(y0)
+        w_ref = consts_l.w_max * np.random.random((N,))
+        _, stats_l = price_solver_l.compute_optimal_prices(w_ref, lmbd_r)
+        _, stats_lc = price_solver_lc.compute_optimal_prices(w_ref, lmbd_r)
         avg_price_l = avg_price_l + stats_l["price_after_reg"]
         avg_price_lc = avg_price_lc + stats_lc["price_after_reg"]
     avg_price_l = avg_price_l / nsamples
@@ -204,11 +211,11 @@ def main() -> None:
     consts = get_chargingstation_consts()
 
     # Plots.
-    _plot_robustness_bounds_large_robots(consts)
+    # _plot_robustness_bounds_large_robots(consts)
     _plot_dual_cost_decrease_large_robots(consts)
 
     # Statistics: Price comparison.
-    _price_comparison_large_robots(consts)
+    # _price_comparison_large_robots(consts)
 
 
 if __name__ == "__main__":
